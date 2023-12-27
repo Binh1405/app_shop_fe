d0c783f3a0dbe0b8a0506d2b4fe65252617281c0
khanhnd <khanhnd@nexlesoft.com>
2023-12-27 20:49:58 +0700

    update-ui-login-register

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,7 @@
 'use client'
-import { Typography, useTheme } from '@mui/material'
 import Head from 'next/head'
-import { useSettings } from 'src/hooks/useSettings'
 
 export default function Home() {
-  const theme = useTheme()
-  const { settings } = useSettings()
-
-  console.log('theme', { theme,settings })
-
   return (
     <>
       <Head>
@@ -17,7 +10,6 @@ export default function Home() {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <Typography>Hello world updated</Typography>
     </>
   )
 }
