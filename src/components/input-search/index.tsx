6091260436217e7b66cec01dec182dc28adcbd08
khanhndnexle <khanhnd@nexlesoft.com>
2024-02-08 21:26:21 +0700

    update-type-props-input-search

--- src/components/input-search/index.tsx
+++ src/components/input-search/index.tsx
@@ -10,7 +10,7 @@ import { useDebounce } from 'src/hooks/useDebounce'
 interface TInputSearch {
   value: string
   onChange: (value: string) => void
-  placeholder: string
+  placeholder?: string
 }
 
 const Search = styled('div')(({ theme }) => ({
