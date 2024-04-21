bc85a4271675b6809b44a8842e301f7ee788a402
khanhndnexle <khanhnd@nexlesoft.com>
2024-04-22 04:51:06 +0700

    count-revenue-order

--- src/views/pages/dashboard/index.tsx
+++ src/views/pages/dashboard/index.tsx
@@ -8,12 +8,19 @@ import Spinner from "src/components/spinner"
 import { Box, Grid } from "@mui/material"
 
 // ** Services
-import { getCountAllRecords, getCountProductStatus, getCountProductTypes } from "src/services/report"
+import { getCountAllRecords, getCountProductStatus, getCountProductTypes, getCountRevenueYear } from "src/services/report"
 import CardCountRecords from "src/views/pages/dashboard/components/CardCountRecords"
 import CardProductType from "src/views/pages/dashboard/components/CardProductType"
+import CardCountRevenue from "src/views/pages/dashboard/components/CardCountRevenue"
 
 export interface TCountProductType {
   typeName: string,
+  total: number
+}
+
+export interface TCountRevenue {
+  year: string,
+  month: string,
   total: number
 }
 
@@ -21,6 +28,8 @@ const Dashboard = () => {
     const [loading, setLoading] = useState(false)
     const [countRecords, setCountRecords] = useState<Record<string, number>>({})
     const [countProductTypes, setCountProductTypes] = useState<TCountProductType[]>([])
+    const [countRevenues, setCountRevenues] = useState<TCountRevenue[]>([])
+
     
     // ** Fetch API
     const fetchAllCountRecords = async () => {
@@ -45,18 +54,33 @@ const Dashboard = () => {
         })
       }
 
+      const fetchAllTotalRevenues = async () => {
+        setLoading(true)
+        await getCountRevenueYear().then((res) => {
+          const data = res?.data
+          setLoading(false)
+          setCountRevenues(data)
+        }).catch(e => {
+          setLoading(false)
+        })
+      }
+
       useEffect(() => {
         fetchAllCountRecords()
         fetchAllProductTypes()
+        fetchAllTotalRevenues()
       }, [])
 
     return (
         <Box>
             {loading && <Spinner />}
             <CardCountRecords data={countRecords} />
-            <Grid container>
+            <Grid container spacing={6}>
               <Grid item md={6} xs={12}>
               <CardProductType data={countProductTypes} />
+              </Grid>
+              <Grid item md={6} xs={12}>
+              <CardCountRevenue data={countRevenues} />
               </Grid>
             </Grid>
         </Box>
