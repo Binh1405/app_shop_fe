00b7639e42de2549c457e78c6d23be2cdd299dc2
khanhndnexle <khanhnd@nexlesoft.com>
2024-04-22 06:10:00 +0700

    pie-chart-user-status-order-status

--- src/views/pages/dashboard/index.tsx
+++ src/views/pages/dashboard/index.tsx
@@ -8,10 +8,12 @@ import Spinner from "src/components/spinner"
 import { Box, Grid } from "@mui/material"
 
 // ** Services
-import { getCountAllRecords, getCountProductStatus, getCountProductTypes, getCountRevenueYear } from "src/services/report"
+import { getCountAllRecords, getCountOrderStatus, getCountProductStatus, getCountProductTypes, getCountRevenueYear, getCountUserType } from "src/services/report"
 import CardCountRecords from "src/views/pages/dashboard/components/CardCountRecords"
 import CardProductType from "src/views/pages/dashboard/components/CardProductType"
 import CardCountRevenue from "src/views/pages/dashboard/components/CardCountRevenue"
+import CardCountUserType from "src/views/pages/dashboard/components/CardCountUserType"
+import CardCountOrderStatus from "src/views/pages/dashboard/components/CardCountStatusOrder"
 
 export interface TCountProductType {
   typeName: string,
@@ -25,66 +27,99 @@ export interface TCountRevenue {
 }
 
 const Dashboard = () => {
-    const [loading, setLoading] = useState(false)
-    const [countRecords, setCountRecords] = useState<Record<string, number>>({})
-    const [countProductTypes, setCountProductTypes] = useState<TCountProductType[]>([])
-    const [countRevenues, setCountRevenues] = useState<TCountRevenue[]>([])
+  const [loading, setLoading] = useState(false)
+  const [countRecords, setCountRecords] = useState<Record<string, number>>({})
+  const [countProductTypes, setCountProductTypes] = useState<TCountProductType[]>([])
+  const [countRevenues, setCountRevenues] = useState<TCountRevenue[]>([])
+  const [countUserType, setCountUserType] = useState<Record<number, number>>({} as any)
+  const [countOrderStatus, setCountOrderStatus] = useState<Record<number, number>>({} as any)
 
-    
-    // ** Fetch API
-    const fetchAllCountRecords = async () => {
-        setLoading(true)
-        await getCountAllRecords().then((res) => {
-          const data = res?.data
-          setLoading(false)
-          setCountRecords(data)
-        }).catch(e => {
-          setLoading(false)
-        })
-      }
+  // ** Fetch API
+  const fetchAllCountRecords = async () => {
+    setLoading(true)
+    await getCountAllRecords().then((res) => {
+      const data = res?.data
+      setLoading(false)
+      setCountRecords(data)
+    }).catch(e => {
+      setLoading(false)
+    })
+  }
 
-      const fetchAllProductTypes = async () => {
-        setLoading(true)
-        await getCountProductTypes().then((res) => {
-          const data = res?.data
-          setLoading(false)
-          setCountProductTypes(data)
-        }).catch(e => {
-          setLoading(false)
-        })
-      }
+  const fetchAllProductTypes = async () => {
+    setLoading(true)
+    await getCountProductTypes().then((res) => {
+      const data = res?.data
+      setLoading(false)
+      setCountProductTypes(data)
+    }).catch(e => {
+      setLoading(false)
+    })
+  }
 
-      const fetchAllTotalRevenues = async () => {
-        setLoading(true)
-        await getCountRevenueYear().then((res) => {
-          const data = res?.data
-          setLoading(false)
-          setCountRevenues(data)
-        }).catch(e => {
-          setLoading(false)
-        })
-      }
+  const fetchAllTotalRevenues = async () => {
+    setLoading(true)
+    await getCountRevenueYear().then((res) => {
+      const data = res?.data
+      setLoading(false)
+      setCountRevenues(data)
+    }).catch(e => {
+      setLoading(false)
+    })
+  }
 
-      useEffect(() => {
-        fetchAllCountRecords()
-        fetchAllProductTypes()
-        fetchAllTotalRevenues()
-      }, [])
+  const fetchAllCountUserType = async () => {
+    setLoading(true)
+    await getCountUserType().then((res) => {
+      const data = res?.data
+      setLoading(false)
+      setCountUserType(data?.data)
+    }).catch(e => {
+      setLoading(false)
+    })
+  }
 
-    return (
-        <Box>
-            {loading && <Spinner />}
-            <CardCountRecords data={countRecords} />
-            <Grid container spacing={6}>
-              <Grid item md={6} xs={12}>
-              <CardProductType data={countProductTypes} />
-              </Grid>
-              <Grid item md={6} xs={12}>
-              <CardCountRevenue data={countRevenues} />
-              </Grid>
-            </Grid>
-        </Box>
-    )
+  const fetchAllCountStatusOrder = async () => {
+    setLoading(true)
+    await getCountOrderStatus().then((res) => {
+      const data = res?.data
+      setLoading(false)
+      setCountOrderStatus(
+        data?.data
+      )
+    }).catch(e => {
+      setLoading(false)
+    })
+  }
+
+  useEffect(() => {
+    fetchAllCountRecords()
+    fetchAllProductTypes()
+    fetchAllTotalRevenues()
+    fetchAllCountUserType()
+    fetchAllCountStatusOrder()
+  }, [])
+
+  return (
+    <Box>
+      {loading && <Spinner />}
+      <CardCountRecords data={countRecords} />
+      <Grid container spacing={6}>
+        <Grid item md={6} xs={12}>
+          <CardProductType data={countProductTypes} />
+        </Grid>
+        <Grid item md={6} xs={12}>
+          <CardCountRevenue data={countRevenues} />
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <CardCountUserType data={countUserType} />
+        </Grid>
+        <Grid item md={4} xs={12}>
+          <CardCountOrderStatus data={countOrderStatus} />
+        </Grid>
+      </Grid>
+    </Box>
+  )
 }
 
 export default Dashboard
