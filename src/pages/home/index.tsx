1149fe3b6f7acc32fb605796f24fff86b809502e
khanhndnexle <khanhnd@nexlesoft.com>
2024-05-11 13:45:52 +0700

    update-server-side-rendering

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -2,6 +2,7 @@
 import Head from 'next/head'
 import { ReactNode } from 'react'
 import { getAllProductsPublic } from 'src/services/product'
+import { getAllProductTypes } from 'src/services/product-type'
 import { TProduct } from 'src/types/product'
 
 // layouts
@@ -10,14 +11,26 @@ import LayoutNotApp from 'src/views/layouts/LayoutNotApp'
 // ** Pages
 import HomePage from 'src/views/pages/home'
 
+interface TOptions {
+  label: string
+  value: string
+}
+
 interface TProps {
   products: TProduct[],
-  totalCount: number
+  totalCount: number,
+  productTypes: TOptions[]
+  params: {
+    limit: number
+    page: number
+    order: string
+    productType: string
+  }
 }
-export default function Home(props:TProps) {
-  const {products, totalCount} = props
-  console.log("products", {products})
-  
+
+export default function Home(props: TProps) {
+  const { products, totalCount, params, productTypes } = props
+
   return (
     <>
       <Head>
@@ -26,7 +39,7 @@ export default function Home(props:TProps) {
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.ico' />
       </Head>
-      <HomePage products={products} totalCount={totalCount} />
+      <HomePage products={products} totalCount={totalCount} paramsServer={params} productTypesServer={productTypes} />
     </>
   )
 }
@@ -35,24 +48,50 @@ Home.getLayout = (page: ReactNode) => <LayoutNotApp>{page}</LayoutNotApp>
 Home.guestGuard = false
 Home.authGuard = false
 
-export async function getServerSideProps(context:any) {
+export async function getServerSideProps() {
+  const limit = 10
+  const page = 1
+  const order = "createdAt desc"
   try {
-    const {limit = 2, page = 1} = context.query
-    const res = await getAllProductsPublic({params: {limit: +limit, page: +page}})
+    const productTypes: TOptions[] = []
+    await getAllProductTypes({ params: { limit: -1, page: -1 } })
+      .then(res => {
+        const data = res?.data.productTypes
+        if (data) {
+          data?.map((item: { name: string; _id: string }) => {
+            productTypes.push({ label: item.name, value: item._id })
+          })
+        }
+      })
+    const res = await getAllProductsPublic(
+      { params: { limit: limit, page: page, order, productType: productTypes?.[0]?.value } }
+    )
 
     const data = res?.data
 
     return {
       props: {
         products: data?.products,
-        totalCount: data?.totalCount
+        totalCount: data?.totalCount,
+        productTypes: productTypes,
+        params: {
+          limit,
+          page,
+          order,
+          productType: productTypes?.[0]?.value
+        }
       }
     }
   } catch (error) {
-     return {
+    return {
       props: {
         products: [],
-        totalCount: 0
+        totalCount: 0,
+        params: {
+          limit,
+          page,
+          order
+        }
       }
     }
   }
